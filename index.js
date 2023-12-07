0b8221d150239dc34fd1d70f798d2cfcaef00121
Vinicius Fontes <vinif.santos2002@gmail.com>
2023-12-07 16:19:41 -0300

    Conflito de Merge resolvido

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ import { turboLogger } from "./turboLogger.js";
 console.log("Estou usando o github");
 
 turboLogger("estou aprendendo o fluxo do git", "red");
+
+turboLogger(
+  "Eu consegui criar uma nova mensagem, sem afetar a versão principal do sistema",
+  "darkgreen"
+);
